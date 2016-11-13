cae12722606a5ac650fc7cfd7b6302a8178f6e67
TryHardDood <rsnconfigs@gmail.com>
2016-11-13 22:04:17 +0100

    Upload files

---
+++ custom/js/custom.js
@@ -0,0 +1,28 @@
+$('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrain_width: true,
+    hover: false,
+    gutter: 0,
+    belowOrigin: true,
+    alignment: 'left'
+});
+
+$(".dropdown-content").on({
+    mouseenter: function() {
+        $(this).parent().addClass("active");
+    },
+    mouseleave: function() {
+        $(this).parent().removeClass("active");
+    }
+});
+
+$('.button-collapse').sideNav({
+    menuWidth: 350,
+    edge: 'left',
+    closeOnClick: false,
+    draggable: true
+});
+$('#header').load("ajax/test.html");
+
+console.log(window.location.host + "/web/");
