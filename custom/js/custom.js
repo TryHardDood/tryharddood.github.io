0ae6a46b60aa85eb9d435bda05a9b0333067e546
TryHardDood <rsnconfigs@gmail.com>
2016-11-13 22:50:14 +0100

    Upload files

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -37,7 +37,7 @@ function printLatestCommits() {
     var responseObj = JSON.parse(this.responseText);
     for (var i = 0; i < (responseObj.length > 5 ? 5 : responseObj.length); i++) {
         var obj = responseObj[i];
+
         $('#commits').append('<div class="divider"></div><div class="section"><h5><a href="' + obj.html_url + '" target="_blank">' + obj.commit.author.name + ' at ' + obj.commit.author.date +'</a></h5><p>' + obj.commit.message + '</p></div>')
     }
 }
-getLatestCommitsFor('web');
