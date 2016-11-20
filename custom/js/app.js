b4fe79a08fa21c5bc78eba083db7a24639ebec2b
TryHardDood <rsnconfigs@gmail.com>
2016-11-20 16:25:01 +0100

    Fixed tabs

--- custom/js/app.js
+++ custom/js/app.js
@@ -160,4 +160,18 @@
             transclude: true
         };
     }]);
+
+    app.directive( 'elemReady', function( $parse ) {
+        return {
+            restrict: 'A',
+            link: function( $scope, elem, attrs ) {
+                elem.ready(function(){
+                    $scope.$apply(function(){
+                        var func = $parse(attrs.elemReady);
+                        func($scope);
+                    })
+                })
+            }
+        }
+    })
 }());
